ab6ee7a005596c22e7d7e2525045b27ea21fcdc7
Andrew Glines <aglines@gmail.com>
2017-05-09 15:07:44 -0700

    Add code to successfully populate outputString with intended data from inputString

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,48 @@
 //BUSINESS LOGIC
 var Encryptor = function(inputString){
-
   inputString = inputString.replace(/\W/g, "");
-
   var stringLength = inputString.length;
   var root = Math.sqrt(stringLength);
   var rows = Math.ceil(root);
   var columns = Math.floor(root);
+  // console.log("inputString ", inputString);
+  // console.log("rows = ", rows, "  cols = ", columns);
 
-console.log("inputString ", inputString);
-console.log("rows = ", rows, "  cols = ", columns);
 
+  var encodedBox = [];
+  k = 0;
+  for ( var j = 0 ; j < rows ; j +=1 ) {
+    encodedBox[j] = [];
+    for ( var i = 0 ; i < columns ; i +=1 ) {
+    encodedBox[j][i] = inputString[k];
+    // console.log("j ", j, "  i ", i, encodedBox[i]);
+    k += 1 ;
+    };
+  };
+console.table(encodedBox);
+// console.log(encodedBox[2][1]); // this works
+// DECRYPTOR
+i = j = k = 0;
+var outputString = [];
 
-var encodedBox = [];
-k = 0;
+for ( i = 0 ; i < columns ; i +=1 ) {
+  for ( j = 0 ; j < rows ; j +=1 ) {
 
-for ( var j = 0 ; j < rows ; j +=1 ) {
-  encodedBox[j] = [];
-  for ( var i = 0 ; i < columns ; i +=1 ) {
-  encodedBox[j][i] = inputString[k];
-  // console.log("j ", j, "  i ", i, encodedBox[i]);
-  k += 1 ;
+    if ( typeof(encodedBox[j][i]) != "undefined" ) {
+          outputString[k] = encodedBox[j][i];
+          k += 1;
+    };
   };
 };
 
-console.log(encodedBox);
+// FINALIZE OUTPUT STRING WITH SPACES
+
+
+console.log(outputString);
+
 
 }; // END FRONTEND
+
 
 //FRONTEND
 
